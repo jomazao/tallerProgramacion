0b47eea8d363050ed2e178fe0ddc25ba1e07709a
Jomazao <jomazao@gmail.com>
2018-12-19 10:36:21 -0500

    se agregó una variable

--- programa6.js
+++ programa6.js
@@ -5,6 +5,6 @@ num1=parseInt(prompt("Ingrese el num1"))
 num2=parseInt(prompt("Ingrese el num2"))
 num3=parseInt(prompt("Ingrese el num3"))
 suma=num1+num2+num3
-var mensaje2
 mensaje="la suma entre "+num1+"+"+num2+ "+"+ num3+"="+ suma
 alert(mensaje2)
+var nombre2="CualquierNombre"
